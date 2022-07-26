5d0700eee84599ef0b6f84409e950687be6d89ca
codeslayer <willyanpublic@protonmail.com>
2022-07-26 19:24:22 -0300

    change colorscheme to ayu-dark

--- config.h
+++ config.h
@@ -1,22 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx			= 6;				/* gaps */
-static const unsigned int snap      = 32;				/* snap pixel */
+static const unsigned int gappx	    = 6;        /* gaps */
+static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "iosevka-semibold:size=10" };
 static const char dmenufont[]       = "iosevka-medium:size=10";
-static const char col_gray1[]       = "#051018";
-static const char col_gray2[]       = "#585B70";
-//static const char col_gray2[]       = "#091e2d";
-static const char col_gray3[]       = "#ffffff";
-static const char col_gray4[]       = "#f0f0f0";
-//static const char col_cyan[]      = "#a4b9ef";
-static const char col_cyan[]        = "#ebddaa";
-//static const char col_cyan[]      = "#C6AAE8";
-//static const char col_cyan[]		= "#E07983";
+static const char col_gray1[]       = "#0A0E14";
+static const char col_gray2[]       = "#1D202F";
+static const char col_gray3[]       = "#F9F9F9";
+static const char col_gray4[]       = "#CBCCC6";
+static const char col_cyan[]        = "#E6B450";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -128,4 +125,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
